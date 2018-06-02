187b2cdc8d28e0e624d9484695d013ad0d3d746b
jtpunt <perryjon@oregonstate.edu>
2018-06-02 01:17:27 -0500

    Update dbcon.js

--- project/dbcon.js
+++ project/dbcon.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
-  host            : '99.189.11.11',
-  user            : 'CS290',
-  password        : '1ch33s31',
-  database        : 'website'
+  host            : 'xx.xxx.xx.xx',
+  user            : 'username',
+  password        : 'password',
+  database        : 'databasename'
 });
 module.exports.pool = pool;
