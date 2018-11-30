020e433d1f0dc8e9c565d4e98e44470f9d6e0b7b
jtpunt <perryjon@oregonstate.edu>
2018-11-30 12:15:18 -0600

    Update charts.js

--- sensor/routes/charts.js
+++ sensor/routes/charts.js
@@ -7,23 +7,10 @@ router.get("/", function(req, res){
         if(err) console.log(err);
         else{
           chart.sort(function(a,b){
-            // Turn your strings into dates, and then subtract them
-            // to get a value that is either negative, positive, or zero.
             return new Date(b.date) - new Date(a.date);
           });
           res.render("charts", {charts: chart, scripts: ["/static/js/drawCharts.js"]});
         }
     });
-	// Chart.find({}, {"_id" : false}, function(err, chart){ //remove _id from query result
- //        if(err) console.log(err);
- //        else{
- //        	Chart.aggregate([{ "$group": { "_id": "$_refID", "total" : { "$sum": 1 }}}], // maybe instead of the refID, we sort by the pin #
- //   				 function(err,docs) {
- //      				if (err) console.log(err);
- //      					console.log( docs );
- //      					res.render("charts", {charts: chart, scripts: ["/static/js/drawCharts.js"]});
- //    		});
- //        }
- //    });
 });
 module.exports = router;
